e80d4ec7b50149dad132115ca81e58c627dab41d
JumMy777 <jummy1806@gmail.com>
2024-04-28 21:31:23 +0800

    added logout and route by joren

--- my-app/src/js/router/router.js
+++ my-app/src/js/router/router.js
@@ -1,41 +1,60 @@
 function setRouter() {
+  // Check the current page
   switch (window.location.pathname) {
-    //! restrict and redirect if logged in using case
-    case "/":
+    // If you are logged in
+    case "/login.html":
+    case "/signup.html":
+      // If the user is logged in
       if (localStorage.getItem("access_token")) {
-        window.location.pathname = "/index.html";
+        // If the user is a customer, redirect them to the index page
+        if (localStorage.getItem("role") == "customer") {
+          window.location.pathname = "/index.html";
+        }
+        // If the user is an admin, redirect them to the admin page
+        else if (localStorage.getItem("role") == "admin") {
+          window.location.pathname = "/admin.html";
+        }
+        // If the user is a hadmin, redirect them to the hadmin page
+        else if (localStorage.getItem("role") == "hadmin") {
+          window.location.pathname = "/hadmin.html";
+        }
       }
       break;
 
-    // !! redirect if not logged in using case
+    // If you are not logged in
     case "/admin.html":
-    case "/admin_hotel.html":
-      // Add more pages to add restrictions
-
-      if (!localStorage.getItem("access_token")) {
-        window.location.pathname = "/index.html";
+      // If the user is not logged in or not an admin, redirect them to the account page
+      if (
+        !localStorage.getItem("access_token") ||
+        localStorage.getItem("role") != "admin"
+      ) {
+        window.location.pathname = "/account.html";
       }
       break;
 
-    // !! redirect if  admin
-    case "/index.html": // Change this to a page where Admin has access; Add more case if there are more pages
-      if (!localStorage.getItem("role") || localStorage.getItem("role") == "admin") {
-        window.location.pathname = "/admin.html";
-      }
-      break;
-
-      // !! redirect if customer
-    case "/index.html": // Change this to a page where customer has access; Add more case if there are more pages
-   
-    if (!localStorage.getItem("role") || localStorage.getItem("role") == "customer") {
-      window.location.pathname = "/index.html";
-    }
-    break;
-
+    // If you are not logged in
+    // case "/hadmin.html":
+    // If the user is not logged in or not a hadmin, redirect them to the account page
+    // if (
+    //   !localStorage.getItem("access_token") ||
+    //   localStorage.getItem("role") != "hadmin"
+    // ) {
+    //   window.location.pathname = "/account.html";
+    // }
+    // break;
 
     default:
-      break;
+      // If the user is an admin, redirect them to the admin page
+      if (localStorage.getItem("role") == "admin") {
+        window.location.pathname = "/admin.html";
+      }
+    // If the user is a hadmin, redirect them to the hadmin page
+    // else if (localStorage.getItem("role") == "hadmin") {
+    //   window.location.pathname = "/hadmin.html";
+    // }
+    // break;
   }
 }
 
+// Export the function
 export { setRouter };
